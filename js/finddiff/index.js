7f95ccd84717a1078eae768988ec959093b754f2
jianfeng.li <jianfeng.li@100credit.com>
2020-07-29 20:07:07 +0800

    添加素材

--- js/finddiff/index.js
+++ js/finddiff/index.js
@@ -1,7 +1,7 @@
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 957,
     title: '来找茬',
     url: 'https://github.com/basebase',
     version: '1.0',
@@ -27,64 +27,69 @@ function preload() {
     this.load.image('lb1', 'lb1.jpeg')
     this.load.image('lb2', 'lb2.jpg')
     this.load.image('lb3', 'lb3.jpg')
+
+    this.load.image('sc1', 'sc1.jpg')
 }
 
 var spriteArray = []
 
 function create() {
-    let i = 0
-    let x = 1
-    let y = 1
-    while (i < 6) {
 
-        if (x * 100 + 100 >= 800) {
-            y = y * 2.5
-            x = 1
-            let tmpSprite = this.add.sprite(x * 100, y * 140, 'lb1').setInteractive()
-        } else {
-            let tmpSprite = this.add.sprite(x * 120, y * 140, 'lb1').setInteractive()
-            tmpSprite.setDisplaySize(100, 200)
-            tmpSprite.setName("K" + i)
-            spriteArray.push(tmpSprite)
-            x ++
-        }
+    this.add.image(500, 510, 'sc1')
 
-        i ++
-    }
+    // let i = 0
+    // let x = 1
+    // let y = 1
+    // while (i < 6) {
 
-    let count = 0
-    let diffImageArray = []
+    //     if (x * 100 + 100 >= 800) {
+    //         y = y * 2.5
+    //         x = 1
+    //         let tmpSprite = this.add.sprite(x * 100, y * 140, 'lb1').setInteractive()
+    //     } else {
+    //         let tmpSprite = this.add.sprite(x * 120, y * 140, 'lb1').setInteractive()
+    //         tmpSprite.setDisplaySize(100, 200)
+    //         tmpSprite.setName("K" + i)
+    //         spriteArray.push(tmpSprite)
+    //         x ++
+    //     }
 
-    spriteArray.forEach(function(sprite, index) {
-        sprite.on('pointerdown', function(pointer) {
-            this.setTint(0xff0000)
-            count ++
-            diffImageArray.push(sprite)
-        })
+    //     i ++
+    // }
 
-        sprite.on('pointerup', function(pointer) {
-            if (count == 2) {
-                console.log("sprite array ", spriteArray)
-                // console.log("sprite array ", ...spriteArray)
-                let spriteV1 = diffImageArray[0]
-                let spriteV2 = diffImageArray[1]
-                state = diffImage(diffImageArray)
-                if (state) {
-                    spriteV1.destroy()
-                    spriteV2.destroy()
+    // let count = 0
+    // let diffImageArray = []
 
-                    spriteArray.splice(spriteArray.indexOf(diffImageArray[0]), 1)
-                    spriteArray.splice(spriteArray.indexOf(diffImageArray[1]), 1)
-                } else {
-                    spriteV1.clearTint()
-                    spriteV2.clearTint()
-                }
+    // spriteArray.forEach(function(sprite, index) {
+    //     sprite.on('pointerdown', function(pointer) {
+    //         this.setTint(0xff0000)
+    //         count ++
+    //         diffImageArray.push(sprite)
+    //     })
 
-                diffImageArray = []
-                count = 0
-            }
-        })
-    })    
+    //     sprite.on('pointerup', function(pointer) {
+    //         if (count == 2) {
+    //             console.log("sprite array ", spriteArray)
+    //             // console.log("sprite array ", ...spriteArray)
+    //             let spriteV1 = diffImageArray[0]
+    //             let spriteV2 = diffImageArray[1]
+    //             state = diffImage(diffImageArray)
+    //             if (state) {
+    //                 spriteV1.destroy()
+    //                 spriteV2.destroy()
+
+    //                 spriteArray.splice(spriteArray.indexOf(diffImageArray[0]), 1)
+    //                 spriteArray.splice(spriteArray.indexOf(diffImageArray[1]), 1)
+    //             } else {
+    //                 spriteV1.clearTint()
+    //                 spriteV2.clearTint()
+    //             }
+
+    //             diffImageArray = []
+    //             count = 0
+    //         }
+    //     })
+    // })    
 }
 
 
